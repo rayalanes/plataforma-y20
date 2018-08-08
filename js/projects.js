56e1d1851afbe11af704a9a9a51baa4ec2c67857
Rodrigo Alfonso <rodri042@gmail.com>
2018-08-08 04:23:58 -0300

    Obteniendo projects por página

--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", (event) => { 
   const SPREADSHEET_ID = "1DEixnU-4WWf4TT2xgbJnQOomcGR6MKmm4kEOtFUKUVs"; // https://docs.google.com/spreadsheets
   const API_KEY = "AIzaSyCzMyPMqKKiaUTywH1VC7CTUnn-eS4g2jQ"; // https://console.developers.google.com/apis/credentials
-  const COLUMN_IMAGE = "B";
-  const COLUMN_NAME = "C";
-  const COLUMN_HASHTAGS = "D";
-  const COLUMN_SDG = "E";
-  const COLUMN_BUDGET = "F";
-  const COLUMN_COMPLEXITY = "G";
+  const COLUMN_IMAGE = 1;
+  const COLUMN_NAME = 2;
+  const COLUMN_HASHTAGS = 3;
+  const COLUMN_SDG = 4;
+  const COLUMN_BUDGET = 5;
+  const COLUMN_COMPLEXITY = 6;
+  const PROJECTS_PER_PAGE = 4;
 
   class Spreadsheet {
     constructor(id, apiKey) {
@@ -27,11 +28,42 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
   }
 
-  // new Spreadsheet(SPREADSHEET_ID, API_KEY).get("A1:Z4").then(function(data) {
-  //   debugger;
-  // });
+  class Project {
+    constructor(values) {
+      this.imageUrl = values[COLUMN_IMAGE];
+      this.name = values[COLUMN_NAME];
+      this.hashtags = values[COLUMN_HASHTAGS];
+      this.sdg = values[COLUMN_SDG];
+      this.budget = values[COLUMN_BUDGET];
+      this.complexity = values[COLUMN_COMPLEXITY];
+    }
+  }
+
+  class ProjectsApi extends Spreadsheet {
+    constructor() {
+      super(SPREADSHEET_ID, API_KEY);
+    }
+
+    getPage(page = 0) {
+      const start = page * PROJECTS_PER_PAGE + 1 /* (to 1-based index) */ + 1 /* (omit header row) */;
+      const end = start + PROJECTS_PER_PAGE - 1 /* (inclusive interval) */;
+
+      return this
+        .get(`A${start}:Z${end}`)
+        .then(({ values }) =>
+          values.map((it) => new Project(it))
+        );
+    }
+  }
 
   angular.module('ProjectsApp', []).controller('ProjectsController', ($scope) => {
-    $scope.greeting = "Hello World";
+    const api = new ProjectsApi();
+
+    $scope.page = 0;
+    $scope.projects = [];
+
+    api.getPage(0).then((projects) => {
+      debugger;
+    });
   });
 });
