76f64518de9bd4d955b9f313b6dc41549ef754c4
Rodrigo Alfonso <rodri042@gmail.com>
2018-08-09 18:17:01 -0300

    Usando un formato más cercano al final del form

--- js/projects.js
+++ js/projects.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => { 
   const SPREADSHEET_ID = "1DEixnU-4WWf4TT2xgbJnQOomcGR6MKmm4kEOtFUKUVs"; // https://docs.google.com/spreadsheets
   const API_KEY = "AIzaSyCzMyPMqKKiaUTywH1VC7CTUnn-eS4g2jQ"; // https://console.developers.google.com/apis/credentials
-  const COLUMN_IMAGE = 1;
+  
   const COLUMN_NAME = 2;
   const COLUMN_DESCRIPTION = 3;
-  const COLUMN_HASHTAGS = 4;
-  const COLUMN_SDG = 5;
-  const COLUMN_BUDGET = 6;
-  const COLUMN_COMPLEXITY = 7;
+  const COLUMN_SDG1 = 4;
+  const COLUMN_IMAGE = 5;
+  const COLUMN_SDG2 = 6;
+  const COLUMN_SDG3 = 7;
+  const COLUMN_TASK_FORCE = 8;
+  const COLUMN_BUDGET = 9;
+  const COLUMN_READINESS = 10;
+  const COLUMN_KEYWORDS = 11;
   const PROJECTS_PER_PAGE = 4;
 
   class Spreadsheet {
@@ -30,18 +34,30 @@ document.addEventListener("DOMContentLoaded", (event) => {
   }
 
   class Project {
+    static readinessOptions() {
+      return [
+        "We need your brain in action!",
+        "Some adjustments required",
+        "Ready to go!"
+      ];
+    }
+
     constructor(values) {
       this.imageUrl = values[COLUMN_IMAGE];
       this.name = values[COLUMN_NAME];
       this.description = values[COLUMN_DESCRIPTION];
-      this.hashtags = values[COLUMN_HASHTAGS];
-      this.sdg = values[COLUMN_SDG];
+      this.keywords = values[COLUMN_KEYWORDS];
+      this.sdg1 = values[COLUMN_SDG1];
+      this.sdg2 = values[COLUMN_SDG2];
+      this.sdg3 = values[COLUMN_SDG3];
       this.budget = values[COLUMN_BUDGET];
-      this.complexity = values[COLUMN_COMPLEXITY];
+      this.readiness = values[COLUMN_READINESS];
 
       const $$ = Math.min(this.budget.split("-")[0].split("$").length - 1, 3);
+      const readinessNumber = Project.readinessOptions().indexOf(this.readiness) + 1;
+      this.hashtags = this.keywords.split(" ").map(it => `#${it}`).join(" ");
       this.budgetIconUrl = `img/budget-${$$}.png`;
-      this.complexityIconUrl = `img/complexity-${this.complexity}.png`;
+      this.readinessIconUrl = `img/readiness-${readinessNumber}.png`;
     }
   }
 
